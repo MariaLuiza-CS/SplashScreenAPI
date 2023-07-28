b371adc3d7ab745b92aa02b49fed7f174655e096
MariaLuiza-CS <m.luiza1843@gmail.com>
2023-07-28 08:34:21 -0300

    adding splash screen

--- app/src/main/java/com/example/splashscreenapi/MainActivity.kt
+++ app/src/main/java/com/example/splashscreenapi/MainActivity.kt
@@ -10,11 +10,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.splashscreenapi.ui.theme.SplashScreenAPITheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
+//        installSplashScreen().setKeepOnScreenCondition{
+//            someCondition
+//        }
+
         setContent {
             SplashScreenAPITheme {
                 // A surface container using the 'background' color from the theme
